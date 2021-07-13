cf63fbaf9f3ead8f42734cb0ee46edc1ebdd3a06
Erfan Abedi <erfanabedi.78@gmail.com>
2021-07-14 01:59:10 +0430

    Added KNN classification [untested]

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from math import log2, sqrt
 from heapq import heappop, heappush
 import time
 from typing import Any, Union
+from sklearn.model_selection import KFold
 
 
 half_space = "\u200c"
@@ -71,7 +72,7 @@ class Normalization:
 
 
 class Processing:
-    def __init__(self, docs: pd.DataFrame, has_champion=True, load=False, load_addr=None) -> None:
+    def __init__(self, docs: pd.DataFrame, length='m', has_champion=True, load=False, load_addr=None) -> None:
         self.normal = Normalization()
         self.doc_lengths = dict()
         self.has_champion = has_champion
@@ -80,12 +81,28 @@ class Processing:
             self.docs = pd.read_pickle(load_addr)
         else:
             self.docs = docs
+        
+        if length == "m":
+            pass
+        else:
+            self.docs = self.docs.head(int(length))
+
+        self.docs['id'] -= 1
 
         print("Tokenizing docs")
         start_time = time.time()
         self.docs["idx"] = docs.apply(
             lambda row: self.gen_doc_idx(row['content']), axis=1)
         print("--- %s seconds ---" % (time.time() - start_time))
+
+        if 'topic' not in self.docs.columns:
+            self.docs['topic'] = ''
+        
+        # Inferred Category
+        self.docs['i_cat'] = ''
+
+        # Sort columns to be compliant with the previous code
+        self.docs = self.docs[['id', 'content', 'url', 'idx', 'topic']]
 
         print("Generating tokens")
         start_time = time.time()
@@ -134,7 +151,7 @@ class Processing:
     # Save term frequency in inverse index
     def create_inv_idx(self):
         inv_idx = {token: dict() for token in self.tokens}
-        for id, _, _, idx in self.docs.itertuples(index=False):
+        for id, _, _, idx, _, _ in self.docs.itertuples(index=False):
             for key, value in idx.items():
                 inv_idx[key][id] = self.tf(value)
             self.doc_lengths[id] = self.gen_doc_length(idx)
@@ -220,8 +237,9 @@ class Processing:
 
 
 class Clustering:
-    def __init__(self, p: Processing, cluster_count: int):
-        self.p = p
+    def __init__(self, p3: Processing, cluster_count: int = 2, p2: Processing = None):
+        self.p3 = p3
+        self.p2 = p2
         self.clusters = {}
         self.cluster_count = cluster_count
         self.cluster_distances = {}
@@ -229,8 +247,8 @@ class Clustering:
 
     # K-means clustering on a given data set in the form of dictionaries
     def k_means(self, max_iter=100):
-        self.cluster_centers = self.p.docs.sample(n=self.cluster_count)
-        data = self.p.docs['id']
+        self.cluster_centers = self.p3.docs.sample(n=self.cluster_count)
+        data = self.p3.docs['id']
 
         for _ in range(max_iter):
             # Calculate the distance between each point and each cluster center
@@ -257,48 +275,95 @@ class Clustering:
 
     # Calculating the distance between a point and a cluster center
     def calculate_distance(self, point, center) -> float:
-        return 1 / self.p.doc_cos_similarity(point, center)
+        return 1 / self.p3.doc_cos_similarity(point, center)
 
     # Finds the document with the least distance to the average of the cluster
     def calculate_center(self, cluster) -> str:
         acc = dict()
         for id in cluster:
-            for word in self.p.docs['idx'][id].keys():
+            for word in self.p3.docs['idx'][id].keys():
                 acc[word] = acc.get(word, 0) + \
-                    self.p.docs['idx'][id][word] / len(cluster)
+                    self.p3.docs['idx'][id][word] / len(cluster)
 
-        self.p.docs.loc['tmp_cl'] = ['tmp_cl', '', '', acc]
+        self.p3.docs.loc['tmp_cl'] = ['tmp_cl', '', '', acc]
 
         sim = dict()
         for id in cluster:
             sim[id] = self.calculate_distance(id, 'tmp_cl')
 
-        self.p.docs.drop('tmp_cl', inplace=True)
+        self.p3.docs.drop('tmp_cl', inplace=True)
         return min(sim, key=sim.get)
 
     def search_docs(self, q: str, search_area: list):
         scores = dict()
-        q_words = self.p.doc_tokens(q, set)
+        q_words = self.p3.doc_tokens(q, set)
         for center in search_area:
             for q_word in q_words:
                 scores[center] = scores.get(
-                    center, 0) + self.p.word_cos_similarity(q_word, center)
+                    center, 0) + self.p3.word_cos_similarity(q_word, center)
         return scores
 
-    def query(self, q, b=2):
-        
+    def query_k_means(self, q, b=2):
+
         cluster_scores = self.search_docs(q, self.cluster_centers)
 
         relevant_centers = reverse_sorted_dict(cluster_scores)[:b]
-        relevant_clusters = [self.clusters[self.cluster_centers.index(center)] for center in relevant_centers]
-        relevant_docs = [self.p.docs[id] for clust in relevant_clusters for id in clust]
+        relevant_clusters = [self.clusters[self.cluster_centers.index(
+            center)] for center in relevant_centers]
+        relevant_docs = [self.p3.docs[id]
+                         for clust in relevant_clusters for id in clust]
 
         scores = self.search_docs(q, relevant_docs)
         return scores
 
+    # def query_knn(self, cat, q):
 
+    # This function performs knn classification algorithm on the given data set.
+    def knn_iteration(self, data: pd.DataFrame, query_id, k, choice_fn=lambda x: max(x, key=x.count)):
+        neighbor_distances = dict()
 
+        # 3. For each example in the data
+        for id in data:
+            # 3.1 Calculate the distance between the query example and the current
+            # example from the data.
+            distance = self.calculate_distance(id, query_id)
             
+            # 3.2 Add the distance and the index of the example to an ordered collection
+            neighbor_distances[id] = distance
+
+        # 4. Sort the ordered collection of distances and indices from
+        # smallest to largest (in ascending order) by the distances
+        sorted_neighbor_distances = reverse_sorted_dict(neighbor_distances)
+
+        # 5. Pick the first K entries from the sorted collection
+        k_nearest_distances = sorted_neighbor_distances[:k]
+
+        # 6. Get the labels of the selected K entries
+        k_nearest_labels = [data[i]['topic'] for _, i in k_nearest_distances]
+
+        # 7. If regression (choice_fn = mean), return the average of the K labels
+        # 8. If classification (choice_fn = mode), return the mode of the K labels
+        return choice_fn(k_nearest_labels)
+
+    def knn_learning(self):
+        k_fold = KFold(10, True, 1)
+        k_scores = dict()
+        k = 0
+        for train, test in k_fold.split(self.p3.docs):
+            k += 1
+            for id in test['id']:
+                test['i_cat'][id] = self.knn_iteration(train['id'], id, k=k)
+            k_scores[k] = len(train[train['topic'] == train['i_cat']]) / len(train)
+            test['i_cat'] = ''
+
+        self.k = max(k_scores, key=k_scores.get)
+
+    def knn_classification(self):
+        for id in self.p2.docs['id']:
+            self.p2.docs['i_cat'][id] = self.knn_iteration(self.p3.docs['id'], id, self.k)
+        self.p2.save_docs() 
+
+
 
 
 ListSet = Union[list, set]
@@ -315,15 +380,12 @@ def main():
     data3 = pd.read_excel("data/phase3/3.xlsx")
 
     data = pd.concat([data1, data2, data3])
+    p3 = Processing(data, length=length, has_champion=False)
+    
+    data = pd.read_excel("data/phase3/data.xlsx")
+    p2 = Processing(data, length=length, has_champion=False)
 
-    if length == "m":
-        data_head = data
-    else:
-        data_head = data.head(int(length))
-
-    data_head['id'] -= 1
-
-    p = Processing(data_head, has_champion=False)
+    c = Clustering(p3=p3, p2=p2)
 
     while(True):
         in_str = input("Enter your query, !q to exit \n")
@@ -332,10 +394,10 @@ def main():
 
         start_time = time.time()
 
-        p.gen_scores(in_str)
-        k = min(5, len(p.scores))
+        p3.gen_scores(in_str)
+        k = min(5, len(p3.scores))
         print("The documents with the best scores are in this order (add +1 to the ids):")
-        print(p.best_k(k, heap_or_sort=True))
+        print(p3.best_k(k, heap_or_sort=True))
         print("Querying took:")
         print("--- %s seconds ---" % (time.time() - start_time))
 
